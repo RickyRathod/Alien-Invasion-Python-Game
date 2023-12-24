2cc39b8ab29e0fb1147705b122334dd2e0abc826
Rick_ <123428369+RickyRathod@users.noreply.github.com>
2023-12-24 18:19:06 -0500

    Refactoring needs to be Finished.

--- Alien_Invasion.py
+++ Alien_Invasion.py
@@ -2,6 +2,10 @@ import sys
 
 import pygame
 
+from settings import Settings
+
+from ship import Ship
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -12,9 +16,12 @@ class AlienInvasion:
         
         #defining the clock
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
         # Set the background color
         self.bg_color = (230, 230, 230)
@@ -23,13 +30,21 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            
+            self._check_events()
+    
+    def _check_events(self):
+            """Respond to keypresses and mouse events."""
+            
+            
             # Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
